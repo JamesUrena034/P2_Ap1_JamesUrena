55262ace31cf89f75422f11352770e5845085a7d
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-10-27 20:44:02 -0400

    Realizando el services

---
+++ P2_Ap1_JamesUrena/P2_Ap1_JamesUrena/Services/RegistroServices.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using P2_Ap1_JamesUrena.DAL;
+using P2_Ap1_JamesUrena.Models;
+
+namespace P2_Ap1_JamesUrena.Services;
+
+public class RegistroServices(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<List<Registros>> GetList(Expression<Func<Registros, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registros
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+
+    }
+}
