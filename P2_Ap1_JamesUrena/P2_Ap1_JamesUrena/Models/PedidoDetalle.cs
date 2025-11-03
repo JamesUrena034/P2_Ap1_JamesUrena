9b02cba40658200a3162b22dad6f5d0c8c234e86
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-03 19:10:59 -0400

    Realizando mi Modelo PedidoDetalles

---
+++ P2_Ap1_JamesUrena/P2_Ap1_JamesUrena/Models/PedidoDetalle.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P2_Ap1_JamesUrena.Models;
+
+public class PedidoDetalle
+{
+    [Key]
+    public int PedidoId { get; set; }
+
+    public int ComponenteId { get; set; }
+
+    public int Cantidad { get; set; }
+
+    public double Precio { get; set; }
+
+
+}
