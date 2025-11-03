51e0daff130f59c4fbe5facb2a8f431627a77db2
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-03 19:09:39 -0400

    realizando mi Modelo Componente

---
+++ P2_Ap1_JamesUrena/P2_Ap1_JamesUrena/Models/Componente.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace P2_Ap1_JamesUrena.Models;
+
+public class Componente
+{
+    [Key]
+    public int ComponenteId { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public decimal Precio { get; set; }
+    public int Existencia { get; set; }
+}
