06d48ae33b0666602d987880ee1ce2b02826767e
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-03 19:08:54 -0400

    Realizando mi modelo Pedidos

---
+++ P2_Ap1_JamesUrena/P2_Ap1_JamesUrena/Models/Pedidos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P2_Ap1_JamesUrena.Models;
+
+public class Pedidos
+{
+    [Key]
+    public int PedidoId { get; set; }
+
+    [Required(ErrorMessage = "La fecha es obligatoria")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+    public string NombreCliente { get; set; } = string.Empty;
+
+    public double Total { get; set; }
+}
