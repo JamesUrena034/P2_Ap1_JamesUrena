9eb6be48c3332bad1cfc76774663d6f9e964a869
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-03 19:08:12 -0400

    Modificando mi Contexto

--- P2_Ap1_JamesUrena/P2_Ap1_JamesUrena/DAL/Contexto.cs
+++ P2_Ap1_JamesUrena/P2_Ap1_JamesUrena/DAL/Contexto.cs
@@ -6,7 +6,38 @@ namespace P2_Ap1_JamesUrena.DAL;
 
 public class Contexto : DbContext
 {
-    public DbSet<Registros> Registros { get; set; }
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Pedidos> Registros { get; set; }
+    public DbSet<PedidoDetalle> PedidoDetalles { get; set; }
+    public DbSet<Componente> Componente { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Componente>(entity =>
+        {
+            entity.HasData(
+                new Componente
+                {
+                    ComponenteId = 1,
+                    Descripcion = "Memoria 4GB",
+                    Precio = 1580,
+                    Existencia = 1
+                },
+                new Componente
+                {
+                    ComponenteId = 2,
+                    Descripcion = "Disco SSD 120MB",
+                    Precio = 4200,
+                    Existencia = 8
+                },
+                new Componente
+                {
+                    ComponenteId = 3,
+                    Descripcion = "Tarjeta de Video",
+                    Precio = 10000,
+                    Existencia = 4
+                });
+        });
+    }
 
 }
